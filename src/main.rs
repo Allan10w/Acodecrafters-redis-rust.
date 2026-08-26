2bf3ecd3791e17de665256896a16f8cc799be5b3
allanchanice <chenxl2280091@stu.nfu.edu.cn>
2026-08-26 09:58:19 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,46 @@
 #![allow(unused_imports)]
 
-use std::io::{Read, Write};
-use std::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment the code below to pass the first stage
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                let mut buf = [0;512];
-                loop {
-                    let bytes_read = stream.read(&mut buf).unwrap();
-                    if bytes_read == 0 {
-                        break;
-                    }
-                    stream.write_all(b"+PONG\r\n").unwrap();
-                }
+    loop {
+        match listener.accept().await {
+            Ok((stream, _address)) => {
+                tokio::spawn(handle_client(stream));
             }
             Err(e) => {
                 println!("error: {}", e);
             }
+        }
+    }
+}
+
+async fn handle_client(mut stream: TcpStream) {
+    let mut buf = [0; 512];
+
+    loop {
+        let bytes_read = match stream.read(&mut buf).await {
+            Ok(bytes_read) => bytes_read,
+            Err(e) => {
+                println!("error reading client: {}", e);
+                break;
+            }
+        };
+
+        if bytes_read == 0 {
+            break;
+        }
+
+        if let Err(e) = stream.write_all(b"+PONG\r\n").await {
+            println!("error writing response: {}", e);
+            break;
         }
     }
 }
